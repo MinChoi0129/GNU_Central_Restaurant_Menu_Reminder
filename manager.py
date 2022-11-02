dca1b593de3a2a619d963563114a43960010e021
MinJae Lee <wjdchs0129@gnu.ac.kr>
2022-11-02 22:22:43 +0900

    .

--- manager.py
+++ manager.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import schedule, requests
 from datetime import datetime
-import json
-import tokenRefresher
+from tokenRefresher import *
+import schedule, requests, json
 
 """MAIN PROGRAM"""
 
@@ -11,23 +10,20 @@ class Type:
     day = ['월요일', '화요일', '수요일', '목요일', '금요일']
     food = ['비빔밥/뚝배기', '양식메뉴', '세트메뉴']
 
-def GNUFoodMessagingService():
-    if tokenRefresher.isRequiredToRefresh_refreshToken():
-        tokenRefresher.refreshRefreshToken()
-    if tokenRefresher.isRequiredToRefresh_accessToken():
-        tokenRefresher.refreshAccessToken()
-    
-    menu: list[str] = [str(bs4_element)[12:-4].split('<br/>')[1:-1] for bs4_element in getSeleniumHTMLFromGNUWebPage()]
-    sendKakaoTalkMessage(menu)
+def GNUFoodMessagingService() -> None:
+    for token_type in ['refresh', 'access']:
+        if isRefreshRequired(token_type): refreshToken(token_type)
+
+    menu_table = [str(bs4_element)[12:-4].split('<br/>')[1:-1] for bs4_element in getSeleniumHTMLFromGNUWebPage()]
+    sendKakaoTalkMessage(menu_table)
     
 def getSeleniumHTMLFromGNUWebPage() -> list:
     options = webdriver.ChromeOptions()
-# 창 숨기는 옵션 추가
     options.add_argument("headless")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     browser = webdriver.Chrome(options=options)
-    browser.get("https://www.gnu.ac.kr/main/ad/fm/foodmenu/selectFoodMenuView.do?mi=1341")
-    
+    browser.get("https://www.gnu.ac.kr/main/ad/fm/foodmenu/selectFoodMenuView.do?mi=1341")  
     browser.execute_script("\
         frm = document.getElementById('detailForm'); \
         frm.restSeq.value = '5';\
@@ -42,40 +38,38 @@ def getSeleniumHTMLFromGNUWebPage() -> list:
     
     return BeautifulSoup(innerhtml, 'html.parser').find("div", "BD_table scroll_gr main").select("tbody > tr")[1].select("div p")[:5]
 
-def sendKakaoTalkMessage(menu):
-    message = generateMenuMesseage(menu)
-    
-    with open("access_token.json","r") as fp:
-        tokens = json.load(fp)
+def sendKakaoTalkMessage(menu_table) -> None:
+    with open("./tokens/access_token.json","r") as fp:
+        token = json.load(fp)
 
-    url="https://kapi.kakao.com/v2/api/talk/memo/default/send"
-
-    headers={"Authorization" : "Bearer " + tokens["access_token"]}
-
-    data={
-        "template_object": json.dumps({
-            "object_type":"text",
-            "text":message,
-            "link":{}
-        })
-    }
+    url = "https://kapi.kakao.com/v2/api/talk/memo/default/send"
+    headers = {"Authorization": "Bearer " + token["access_token"]}
+    data = {"template_object": json.dumps({
+        "object_type": "text",
+        "text": generateMenuMesseage(menu_table),
+        "link": {}
+    })}
 
     response = requests.post(url, headers=headers, data=data)
-    print(response.status_code, end = " ")
-    if response.json().get('result_code') == 0: print('메시지를 성공적으로 보냈습니다.')
-    else: print('메시지를 성공적으로 보내지 못했습니다.' + str(response.json()))
     
-def generateMenuMesseage(menu) -> str:
+    print(datetime.today().strftime("%Y-%m-%d %H:%M:%S"), end = " ")
+    print('(', response.status_code, ')', end = " ")
+    if response.json().get('result_code') == 0: print('전송 성공.\n')
+    else: print('전송 실패.', str(response.json()))
+    
+def generateMenuMesseage(menu_table) -> str:
     txt = ""
     today = datetime.today().weekday()
     txt += "[ 오늘은 " + Type.day[today] + "입니다 ]" + '\n'
-    txt += Type.food[0] + ' : ' + menu[today][0] + '\n'
-    txt += Type.food[1] + ' : ' + menu[today][1] + '\n'
-    txt += Type.food[2] + ' : ' + ''.join(menu[today][3:]) + '\n'
+    txt += Type.food[0] + ' : ' + menu_table[today][0] + '\n'
+    txt += Type.food[1] + ' : ' + menu_table[today][1] + '\n'
+    txt += Type.food[2] + ' : ' + ''.join(menu_table[today][3:]) + '\n'
     return txt
 
-def run():
-    print("Running system...")
+def run() -> None:
+    print("==============================")
+    print("중앙식당알리미를 시작합니다.")
+    print("==============================")
     schedule.every(5).seconds.do(GNUFoodMessagingService)
     while True:
         schedule.run_pending()
--- tokenRefresher.py
+++ tokenRefresher.py
@@ -1,92 +1,67 @@
 import json, requests
 from datetime import datetime
 
-def recordRefreshTime(token_type):
-    f = open("lastTokenRefreshDate.txt", mode='r')
-    access = f.readline()
-    refresh = f.readline()            
-    f.close()
-    
-    f = open("lastTokenRefreshDate.txt", mode='w')        
-    
-    
-    current_time = str(datetime.today())
-    if token_type == 'access':
-        f.write(current_time + '\n' + refresh)
-    elif token_type == 'refresh':
-        f.write(access + current_time)
-    else: raise ValueError
-    
-    f.close()
-    
-    
+def recordRefreshedTime(token_type):
+    with open("./data/lastTokenRefreshDate.txt", mode='r') as f:
+        refresh, access = f.readline().rstrip(), f.readline().rstrip()
 
-def isRequiredToRefresh_refreshToken(): # 45일마다 갱신시 최적
-    f = open("lastTokenRefreshDate.txt", mode='r')
-    access = f.readline().rstrip()
-    refresh = f.readline().rstrip()    
-    f.close()
-    old_time = datetime.strptime(refresh, "%Y-%m-%d %H:%M:%S.%f")
-    current_time = datetime.today()
+    with open("./data/lastTokenRefreshDate.txt", mode='w') as f:
+        current_time = str(datetime.today())
+        updated_text = '\n'.join([refresh, current_time]) if token_type == 'access' else '\n'.join([current_time, access])
+        f.write(updated_text)
+        print("%s_token 발급일을 갱신했습니다." % token_type)
+        print("------------------------------")
+
+def isRefreshRequired(token_type):
+    with open("./data/lastTokenRefreshDate.txt", mode='r') as f:
+        refresh, access = f.readline().rstrip(), f.readline().rstrip()
+        target_token = refresh if token_type == 'refresh' else access
+        
+        old_datetime = datetime.strptime(target_token, "%Y-%m-%d %H:%M:%S.%f")
+        current_datetime = datetime.today()
     
-    passed_days = ((current_time - old_time).total_seconds() / 60 / 60 / 24)
-    print(passed_days >= 45)
-    return passed_days >= 45
+    passed_hours = (current_datetime - old_datetime).total_seconds() / 3600
 
-def isRequiredToRefresh_accessToken(): # 4시간마다 갱신시 최적
-    f = open("lastTokenRefreshDate.txt", mode='r')
-    access = f.readline().rstrip()
-    refresh = f.readline().rstrip()    
-    f.close()
-    old_time = datetime.strptime(access, "%Y-%m-%d %H:%M:%S.%f")
-    current_time = datetime.today()
+    if token_type == 'refresh': need_refresh = passed_hours / 24 >= 45    
+    elif token_type == 'access': need_refresh = passed_hours >= 4
     
-    passed_hours = ((current_time - old_time).total_seconds() / 60 / 60)
-    print(passed_hours >= 4)
-    return passed_hours >= 4
-
-def refreshAccessToken():
-    print("access token을 갱신합니다.")
-    url = "https://kauth.kakao.com/oauth/token"
-
-    with open("normal_token_including_refresh_token.json","r") as fp:
-        tokens = json.load(fp)
+    if need_refresh: print("%s_token이 만료되었습니다." % token_type)
+    else: print("%s_token은 유효합니다." % token_type)
     
-    data = {
-        "grant_type": "refresh_token",
-        "client_id": "ae0853e0034b1459fa968e890540c744",
-        "refresh_token": tokens["refresh_token"]
-    }
-    response = requests.post(url, data=data)
-    tokens = response.json()
+    return need_refresh
 
-    with open("access_token.json", "w") as fp:
-        json.dump(tokens, fp)
-    print(tokens)
-    recordRefreshTime('access')
-    return
-
-def refreshRefreshToken():
-    print("refresh token을 갱신합니다.")
-    # https://kauth.kakao.com/oauth/authorize?client_id=ae0853e0034b1459fa968e890540c744&redirect_uri=https://example.com/oauth&response_type=code
+def refreshToken(token_type):
     url = 'https://kauth.kakao.com/oauth/token'
     rest_api_key = 'ae0853e0034b1459fa968e890540c744'
     redirect_uri = 'https://example.com/oauth'
-    authorize_code = "HJTBNubXXgDQcqlWMJIdsW52gd2FVqbE2K31QE82JBaxmaF6GYpgt76cvLokWjreJuBeoAo9dVoAAAGENivjxA"
-
-    data = {
-        'grant_type':'authorization_code',
-        'client_id':rest_api_key,
-        'redirect_uri':redirect_uri,
-        'code': authorize_code,
+    authorize_code = "YSkYyocg2PwernbLIUSPHNY323qvI05miHp9NVeO33Txv4xTNAVeIVmevB9Ucn17cUg26Ao9dGgAAAGEOBFpwg"
+    # https://kauth.kakao.com/oauth/authorize?client_id=ae0853e0034b1459fa968e890540c744&redirect_uri=https://example.com/oauth&response_type=code
+        
+    if token_type == 'access':
+        with open("./tokens/normal_token_including_refresh_token.json","r") as fp:
+            token = json.load(fp)
+        
+        data = {
+            "grant_type": "refresh_token",
+            "client_id": rest_api_key,
+            "refresh_token": token["refresh_token"]
         }
 
-    response = requests.post(url, data=data)
-    tokens = response.json()
-    print(tokens)
+        with open("./tokens/access_token.json", "w") as fp:
+            json.dump(requests.post(url, data=data).json(), fp)
 
+    elif token_type == 'refresh':
+        data = {
+            'grant_type':'authorization_code',
+            'client_id':rest_api_key,
+            'redirect_uri':redirect_uri,
+            'code': authorize_code,
+        }
 
-    with open("normal_token_including_refresh_token.json","w") as fp:
-        json.dump(tokens, fp)
-        
-    recordRefreshTime('refresh')
+        with open("./tokens/normal_token_including_refresh_token.json","w") as fp:
+            result = requests.post(url, data=data).json()
+            json.dump(result, fp)
+            result_dict = json.loads(result)
+            if 'error' in result_dict: print("갱신 에러", result_dict)
+            else: print("%s_token 갱신을 성공했습니다!" % token_type)
+    recordRefreshedTime(token_type)
